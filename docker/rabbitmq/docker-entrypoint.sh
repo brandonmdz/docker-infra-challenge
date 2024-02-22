f92b87e64706fd6cd88e47282ed5fe06ba680694
brandonmdz <brandondylanzalazar@gmail.com>
2024-02-22 15:02:45 -0300

    fix user issues in rabbitmq

--- docker/rabbitmq/docker-entrypoint.sh
+++ docker/rabbitmq/docker-entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-rabbitmq-plugins enable rabbitmq_management
-
 ( rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
-rabbitmqctl enable_feature_flag classic_mirrored_queue_version ; \
+rabbitmq-plugins enable rabbitmq_management ; \
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
 rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
 rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
