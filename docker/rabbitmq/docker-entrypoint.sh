df118d7f4a1a4693b4fe71638e4325c6f4e13cc2
brandonmdz <brandondylanzalazar@gmail.com>
2024-02-22 14:50:22 -0300

    fix user issues in rabbitmq

--- docker/rabbitmq/docker-entrypoint.sh
+++ docker/rabbitmq/docker-entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 rabbitmq-plugins enable rabbitmq_management
-rabbitmqctl enable_feature_flag classic_mirrored_queue_version
 
-rabbitmq-server -detached
+( rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
+rabbitmqctl enable_feature_flag classic_mirrored_queue_version ; \
+rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
+echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
+echo "*** Log in the WebUI at port 15672 (example: http://localhost:15672) ***") &
 
-sleep 10
-
-rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD
-rabbitmqctl set_user_tags $RABBITMQ_USER administrator
-rabbitmqctl set_permissions -p / $RABBITMQ_USER ".*" ".*" ".*"
-echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***"
-echo "*** Log in the WebUI at port 15672 (example: http://localhost:15672) ***"
-
-rabbitmqctl stop
 exec rabbitmq-server $@
